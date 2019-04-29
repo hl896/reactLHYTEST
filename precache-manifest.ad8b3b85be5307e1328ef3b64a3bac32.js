ccd77841441e981adc72ce21bd874e04da116ca8
yeung.li <yeung.li@hinterlands.tech>
2019-04-29 22:17:03 +1000

    Updates

---
+++ precache-manifest.ad8b3b85be5307e1328ef3b64a3bac32.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c7e09d2c67eedd165dc9d8dab435bbf",
+    "url": "/reactLHYTEST/static/media/pro2.7c7e09d2.jpeg"
+  },
+  {
+    "revision": "4c5aaa74e5fef09d7ef6",
+    "url": "/reactLHYTEST/static/css/main.946d17fc.chunk.css"
+  },
+  {
+    "revision": "3447f8ba097dc7923e7a",
+    "url": "/reactLHYTEST/static/js/runtime~main.3447f8ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactLHYTEST/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f453347a7dd578692a82",
+    "url": "/reactLHYTEST/static/js/2.f453347a.chunk.js"
+  },
+  {
+    "revision": "9bc7d2856ffbc16b68832d397058e2a7",
+    "url": "/reactLHYTEST/static/media/pro4.9bc7d285.jpeg"
+  },
+  {
+    "revision": "695419bf92be322b794e0013e7b0a8b4",
+    "url": "/reactLHYTEST/static/media/2.695419bf.jpeg"
+  },
+  {
+    "revision": "7420fc58dcf1c2d9a481e5ce432acc6d",
+    "url": "/reactLHYTEST/static/media/3.7420fc58.jpeg"
+  },
+  {
+    "revision": "eedefd0e7b6decc71890de208d0ceac0",
+    "url": "/reactLHYTEST/static/media/pexels-photo-839115.eedefd0e.png"
+  },
+  {
+    "revision": "b2ec4567bee5558285a86dffc211c46b",
+    "url": "/reactLHYTEST/static/media/pexels-photo-1470165.b2ec4567.jpeg"
+  },
+  {
+    "revision": "318aed6ceafdc852f98056221732ce2f",
+    "url": "/reactLHYTEST/static/media/pexels-photo-1598505.318aed6c.jpeg"
+  },
+  {
+    "revision": "ceb569bff043bb897fc9de95a04b01df",
+    "url": "/reactLHYTEST/static/media/pro1.ceb569bf.jpeg"
+  },
+  {
+    "revision": "4c5aaa74e5fef09d7ef6",
+    "url": "/reactLHYTEST/static/js/main.4c5aaa74.chunk.js"
+  },
+  {
+    "revision": "f531cf2c26168089fbcadf8823bb7313",
+    "url": "/reactLHYTEST/static/media/pro3.f531cf2c.jpeg"
+  },
+  {
+    "revision": "cb9730e09361ccca0b71ceb9e2d5b30f",
+    "url": "/reactLHYTEST/static/media/rocketchat.cb9730e0.svg"
+  },
+  {
+    "revision": "20b2ca4fac409bbda86bafc62e4c4017",
+    "url": "/reactLHYTEST/static/media/git-square.20b2ca4f.svg"
+  },
+  {
+    "revision": "48a2133dd5c34ad46cc6ab6ff06c03ec",
+    "url": "/reactLHYTEST/static/media/linkedin.48a2133d.svg"
+  },
+  {
+    "revision": "5a0555d2670c5d11b1395a499ba3eefb",
+    "url": "/reactLHYTEST/static/media/weixin.5a0555d2.svg"
+  },
+  {
+    "revision": "f5078d36801b554e337f89d3a5f96c82",
+    "url": "/reactLHYTEST/static/media/WechatIMG176.f5078d36.jpeg"
+  },
+  {
+    "revision": "2905425780009a0dea8d6946c834f60c",
+    "url": "/reactLHYTEST/static/media/0.29054257.jpeg"
+  },
+  {
+    "revision": "07cded5e338e1327fa9b8b748da144e6",
+    "url": "/reactLHYTEST/static/media/1.07cded5e.jpeg"
+  },
+  {
+    "revision": "c1d04630bde23a5891aca7e2884bdcf2",
+    "url": "/reactLHYTEST/static/media/2.c1d04630.jpeg"
+  },
+  {
+    "revision": "f453347a7dd578692a82",
+    "url": "/reactLHYTEST/static/css/2.c803dea2.chunk.css"
+  },
+  {
+    "revision": "481d03a73a31dcd281cd765b5c63e56c",
+    "url": "/reactLHYTEST/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactLHYTEST/precache-manifest.ad8b3b85be5307e1328ef3b64a3bac32.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactLHYTEST/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
